2d1ee0bfe3045214169081c59bcb5e0f76c91815
FooKaDoO <92516727+FooKaDoO@users.noreply.github.com>
2021-11-24 13:47:18 +0200

    lmao

---
+++ buttonpress.js
@@ -0,0 +1,4 @@
+function sorting(sortingalgorithm) {
+    var temp = "<iframe src='" + sortingalgorithm + ".html' width='600' height='600'></iframe>"
+    document.getElementById("sorting").innerHTML = temp;
+}
