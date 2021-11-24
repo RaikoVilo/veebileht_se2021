e8e912c8123c6420262fe970f5bfd3e209665802
RaikoVilo <34316385+RaikoVilo@users.noreply.github.com>
2021-11-24 13:00:46 +0200

    bubble

--- bubble.js
+++ bubble.js
@@ -1,65 +1,80 @@
-function quicksort(array) {
-    if (array.length <= 1) {
-      return array;
+const container = document.querySelector(".data-container");
+
+function generateBlocks(num = 20) {
+  if (num && typeof num !== "number") {
+    alert("First argument must be a typeof Number");
+    return;
+  }
+  for (let i = 0; i < num; i += 1) {
+    const value = Math.floor(Math.random() * 100);
+
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.height = `${value * 3}px`;
+    block.style.transform = `translateX(${i * 30}px)`;
+
+    const blockLabel = document.createElement("label");
+    blockLabel.classList.add("block__id");
+    blockLabel.innerHTML = value;
+
+    block.appendChild(blockLabel);
+    container.appendChild(block);
+  }
+}
+
+function swap(el1, el2) {
+  return new Promise(resolve => {
+    const style1 = window.getComputedStyle(el1);
+    const style2 = window.getComputedStyle(el2);
+
+    const transform1 = style1.getPropertyValue("transform");
+    const transform2 = style2.getPropertyValue("transform");
+
+    el1.style.transform = transform2;
+    el2.style.transform = transform1;
+
+    // Wait for the transition to end!
+    window.requestAnimationFrame(function() {
+      setTimeout(() => {
+        container.insertBefore(el2, el1);
+        resolve();
+      }, 250);
+    });
+  });
+}
+
+async function bubbleSort(delay = 100) {
+  if (delay && typeof delay !== "number") {
+    alert("sort: First argument must be a typeof Number");
+    return;
+  }
+  let blocks = document.querySelectorAll(".block");
+  for (let i = 0; i < blocks.length - 1; i += 1) {
+    for (let j = 0; j < blocks.length - i - 1; j += 1) {
+      blocks[j].style.backgroundColor = "#FF4949";
+      blocks[j + 1].style.backgroundColor = "#FF4949";
+
+      await new Promise(resolve =>
+        setTimeout(() => {
+          resolve();
+        }, delay)
+      );
+
+      const value1 = Number(blocks[j].childNodes[0].innerHTML);
+      const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
+
+      if (value1 > value2) {
+        await swap(blocks[j], blocks[j + 1]);
+        blocks = document.querySelectorAll(".block");
+      }
+
+      blocks[j].style.backgroundColor = "#58B7FF";
+      blocks[j + 1].style.backgroundColor = "#58B7FF";
     }
-  
-    var pivot = array[0];
-    
-    var left = []; 
-    var right = [];
-  
-    for (var i = 1; i < array.length; i++) {
-      array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
-    }
-  
-    return quicksort(left).concat(pivot, quicksort(right));
-  };
-  
-  var unsorted = [23, 45, 16, 37, 3, 99, 22];
-  var sorted = quicksort(unsorted);
-  
-  console.log('Sorted array', sorted);
 
-
-  function swap(arr, xp, yp)
-{
-    var temp = arr[xp];
-    arr[xp] = arr[yp];
-    arr[yp] = temp;
-}
- 
-//-----------------------------------------------------------------------------------------------------------------------------------------
-
-function bubbleSort( arr, n)
-{
-var i, j;
-for (i = 0; i < n-1; i++)
-{
-    for (j = 0; j < n-i-1; j++)
-    {
-        if (arr[j] > arr[j+1])
-        {
-        swap(arr,j,j+1);
-         
-        }
-    }
- 
-}
+    blocks[blocks.length - i - 1].style.backgroundColor = "#13CE66";
+  }
 }
 
-function printArray(arr, size)
-{
-    var i;
-    for (i=0; i < size; i++)
-        document.write(arr[i]+ " ");
-    document.write("\n");
-}
-
-var arr = [64, 34, 25, 12, 22, 11, 90];
-var n = 7;
-document.write("UnSorted array: \n");
-printArray(arr, n);
-
-bubbleSort(arr, n);
-document.write("Sorted array: \n");
-printArray(arr, n);
+generateBlocks();
+bubbleSort();
